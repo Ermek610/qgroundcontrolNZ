382f7a246778f6e683891acc51c04c57a69bd630
Ermek610 <ouzuru57@gmail.com>
2022-05-13 17:04:33 +0600

    ElevationMapProvider.h update

--- src/QtLocationPlugin/ElevationMapProvider.h
+++ src/QtLocationPlugin/ElevationMapProvider.h
@@ -42,4 +42,48 @@ class AirmapElevationProvider : public ElevationProvider {
   protected:
     QString _getURL(const int x, const int y, const int zoom, QNetworkAccessManager* networkManager) override;
 };
+// Geoserver Elevation
+
+class GeoserverElevationProvider : public ElevationProvider {
+    Q_OBJECT
+  public:
+    GeoserverElevationProvider(QString geoserverURL, QObject* parent = nullptr);
+
+    enum WMSVersion{
+        V1_0_0,
+        V1_1_0,
+        V1_1_1,
+        V1_3_0
+    };
+
+    QString wmtsVersionString();
+    QString formatURL(QString url);
+    QString geoserverURL() const;
+    void setGeoserverURL(const QString &geoserverURL);
+    QString getWorkspace() const;
+    void setWorkspace(const QString &workspace);
+    QString getMapName() const;
+    void setMapName(const QString &mapName);
+    WMSVersion wmtsVersion() const;
+    void setWmtsVersion(const WMSVersion &wmsVersion);
+    unsigned long long getEPSGNumber() const;
+    void setEPSGNumber(unsigned long long EPSGNumber);
+
+    QGCTileSet getTileCount(const int zoom, const double topleftLon,
+                            const double topleftLat, const double bottomRightLon,
+                            const double bottomRightLat) const override;
+    virtual QNetworkRequest getTileURL(const int x, const int y, const int zoom, QNetworkAccessManager* networkManager) override;
+  protected:
+    QString _getURL(const int x, const int y, const int zoom, QNetworkAccessManager* networkManager) override;
+protected:
+    static const QString m_serviceType;
+    static const QString m_requestType;
+    QString m_geoserverURL;
+    QString m_workspace;
+    QString m_mapName;
+    WMSVersion m_wmtsVersion;
+    unsigned long long m_EPSGNumber;
+    QString m_imageFormat;
+};
+
 
